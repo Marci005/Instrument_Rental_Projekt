78699473affb20e1ce92bfc2d9b8d5b29ab247e7
Marci005 <toth.marton@blathy.info>
2026-03-26 15:54:47 +0100

    default varchar length modified in AppServiceProvider.php And filled the users table in 0001_01_01_000000_create_users_table.php

--- Instrument_Rental/database/migrations/0001_01_01_000000_create_users_table.php
+++ Instrument_Rental/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_admin')->default(false);
+            $table->string('email', 100)->unique();
+            $table->string('password', 100)->nullable(false);
+            $table->enum('title', ['Úr', 'Hölgy', 'Doktor', 'Professzor']);
+            $table->string('first_name', 50)->nullable(false);
+            $table->string('last_name', 50)->nullable(false);
             $table->timestamps();
+
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
