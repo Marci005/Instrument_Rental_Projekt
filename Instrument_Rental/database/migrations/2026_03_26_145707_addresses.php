708d091fd99ca15e3c025fe33adab4c7be4ee6d1
Marci005 <toth.marton@blathy.info>
2026-03-26 16:05:54 +0100

    addresses table filled

--- Instrument_Rental/database/migrations/2026_03_26_145707_addresses.php
+++ Instrument_Rental/database/migrations/2026_03_26_145707_addresses.php
@@ -11,7 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('addresses', static function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('address_type', ['számlázási', 'szállítási', 'mindkettő']);
+            $table->integer('zip', 50)->nullable(false);
+            $table->string('settlement', 100)->nullable(false);
+            $table->string('street', 100)->nullable(false);
+            $table->string('street_type', 30)->nullable(false);
+            $table->string('house_number', 100)->nullable(false);
+            $table->smallInteger('floor_number', 20)->nullable();
+            $table->smallInteger('door_number', 20)->nullable();
+            $table->timestamps();
+
+
+
+        });
     }
 
     /**
