9cd01ea9c7b29b2cfb82b7cdf0b17642c2bfe7e9
Marci005 <toth.marton@blathy.info>
2026-03-26 16:11:18 +0100

    instrument_categories table filled

--- Instrument_Rental/database/migrations/2026_03_26_150825_instrument_categories.php
+++ Instrument_Rental/database/migrations/2026_03_26_150825_instrument_categories.php
@@ -11,7 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('instrument_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name',100)->nullable(false);
+            $table->string('category_description');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('instrument_categories');
     }
 };
