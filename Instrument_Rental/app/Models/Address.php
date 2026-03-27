df27c6c02e862aa1ca6496543369d07592476d01
Marci005 <toth.marton@blathy.info>
2026-03-27 12:37:42 +0100

    Addresses filled

--- Instrument_Rental/app/Models/Address.php
+++ Instrument_Rental/app/Models/Address.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'country',
+        'zip',
+        'street',
+        'house_number',
+    ];
+    /**
+     * Connects this model to the User model's user_id
+     *
+     * @return
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
