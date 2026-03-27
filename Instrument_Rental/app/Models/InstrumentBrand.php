deefa3e1ef0e4a562257265b3ed568880436c34d
Marci005 <toth.marton@blathy.info>
2026-03-27 12:52:03 +0100

    InstrumentBrand crated and filled

---
+++ Instrument_Rental/app/Models/InstrumentBrand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class InstrumentBrand extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+
+    protected $fillable = [
+        'brand_name',
+        'brand_description',
+    ];
+
+    /**
+     * Connects the IsntrumentBrand model the Instruments Model with HasMany relation
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+
+    public function instruments(): HasMany
+    {
+     return $this->hasMany(Instrument::class, 'brand_id');
+    }
+}
