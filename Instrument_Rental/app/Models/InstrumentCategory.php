45387e38a66bdec16b6e2922626f22e5f04ab1b4
Marci005 <toth.marton@blathy.info>
2026-03-27 12:56:02 +0100

    InstrumentCategory filled

--- Instrument_Rental/app/Models/InstrumentCategory.php
+++ Instrument_Rental/app/Models/InstrumentCategory.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class InstrumentCategory extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+
+    protected $fillable = [
+        'category_name',
+        'category_description',
+    ];
+
+    /**
+     * Connects the InstrumentCategory model the Instruments Model with HasMany relation
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function instruments(): HasMany
+    {
+        return $this->hasMany(Instrument::class, 'category_id');
+    }
+
 }
