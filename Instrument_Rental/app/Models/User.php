a836c524437d34d65f8dd3b4b2ec8694b60a37c0
Marci005 <toth.marton@blathy.info>
2026-03-27 12:28:35 +0100

    User.php filled

--- Instrument_Rental/app/Models/User.php
+++ Instrument_Rental/app/Models/User.php
@@ -10,8 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
@@ -21,6 +20,32 @@ class User extends Authenticatable
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
+     */
+
+    /**
+     * Attributes that can be filled by the user
+     * @var string[]
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Attributes that should be hidden for arrays and JSON
+     * @var string[]
+     */
+    protected $hidden = [
+        'is_admin',
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * gives back the time when the email has been verifyed, plus the password hashed
+     * @return string[]
      */
     protected function casts(): array
     {
