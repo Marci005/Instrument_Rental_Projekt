47b97401132f562ab08b749ded185a94dc10e6f7
Marci005 <toth.marton@blathy.info>
2026-03-27 13:04:43 +0100

    Instrument Model created

--- Instrument_Rental/app/Models/Instrument.php
+++ Instrument_Rental/app/Models/Instrument.php
@@ -3,10 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Instrument extends Model
 {
-    protected $fillable = [
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
 
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'condition',
+        'title',
+        'description',
     ];
+
+    /**
+     * Connects the InstrumentB model the InstrumentCategory Model with BelongsTo relation
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(InstrumentCategory::class, 'category_id');
+    }
+
+    /**
+     * Connects the InstrumentB model the InstrumentBrand Model with BelongsTo relation
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(InstrumentBrand::class, 'brand_id');
+    }
 }
