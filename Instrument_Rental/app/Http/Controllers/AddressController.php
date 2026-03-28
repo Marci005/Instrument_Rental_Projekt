1cd6c2cd70ac5f555ee6c2d25cad0fcd049a9541
Marci005 <toth.marton@blathy.info>
2026-03-28 20:31:27 +0100

    AddressController.php index function created

--- Instrument_Rental/app/Http/Controllers/AddressController.php
+++ Instrument_Rental/app/Http/Controllers/AddressController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    //
+
+    /**
+     * GET /api/addresses
+     * The logged in user's data
+     */
+    public function index(Request $request){
+
+
+        return response()->json($request->user()->addresses);
+    }
 }
