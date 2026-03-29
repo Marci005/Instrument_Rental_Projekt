7ef75d22c383f24ab19989a8175f4a08f1aedca6
Marci005 <toth.marton@blathy.info>
2026-03-29 23:32:33 +0200

    InstrumentCategoryController.php filled

--- Instrument_Rental/app/Http/Controllers/InstrumentCategoryController.php
+++ Instrument_Rental/app/Http/Controllers/InstrumentCategoryController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InstrumentCategory;
 use Illuminate\Http\Request;
 
 class InstrumentCategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(InstrumentCategory::all());
+    }
+
+    public function show(InstrumentCategory $instrumentCategory)
+    {
+        return response()->json($instrumentCategory->load('instruments'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'category_name' => 'required|string|max:100',
+            'category_description' => 'nullable|string',
+        ]);
+
+        $category = InstrumentCategory::create($data);
+
+        return response()->json($category, 201);
+
+    }
+
+    public function update(Request $request, InstrumentCategory $instrumentCategory)
+    {
+        $data = $request->validate([
+            'category_name' => 'sometimes|required|string|max:100',
+            'category_description' => 'nullable|string',
+        ]);
+
+        $instrumentCategory->update($data);
+
+        return response()->json($instrumentCategory);
+    }
+
+    public function destroy(InstrumentCategory $instrumentCategory)
+    {
+        $instrumentCategory->delete();
+
+        return response()->json(['message' => 'Kategória törölve']);
+    }
+
+
 }
