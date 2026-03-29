6cd46d7530f41b470ca8970224b3a25b76ed2c0f
Marci005 <toth.marton@blathy.info>
2026-03-29 20:54:12 +0200

    IsAdmin.php commented

--- Instrument_Rental/app/Http/middleware/IsAdmin.php
+++ Instrument_Rental/app/Http/middleware/IsAdmin.php
@@ -7,6 +7,15 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class IsAdmin
+ *
+ * Middleware to restrict access to admin-only routes.
+ * Returns a 403 JSON response if the user is not authenticated
+ * or does not have admin privileges.
+ *
+ * @package App\Http\Middleware
+ */
 class IsAdmin
 {
   public function handle (Request $request, Closure $next): Response
