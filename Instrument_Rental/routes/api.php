25c72c9f0f88182fa949251fb1c3e64587f94c24
Marci005 <toth.marton@blathy.info>
2026-03-29 23:19:48 +0200

    []-s added to the middleware Route to fix its issue which was not calling the middleware alias is_admin

--- Instrument_Rental/routes/api.php
+++ Instrument_Rental/routes/api.php
@@ -56,7 +56,7 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::get('/instrument_brands', [InstrumentBrandController::class, 'index']);
 Route::get('/instrument_brands/{brand}', [InstrumentBrandController::class, 'show']);
 
-Route::middleware('auth:sanctum', 'is_admin')->group(function () {
+Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
     Route::post('/instrument_brands', [InstrumentBrandController::class, 'store']);
     Route::put('/instrument_brands/{InstrumentBrand}', [InstrumentBrandController::class, 'update']);
     Route::patch('/instrument_brands/{InstrumentBrand}', [InstrumentBrandController::class, 'update']);
