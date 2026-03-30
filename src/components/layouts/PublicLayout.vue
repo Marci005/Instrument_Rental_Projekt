a4b521493c5b6cf860610e8e3ee6b086cec6b174
user <user@DESKTOP-51547N5>
2026-03-30 18:26:44 +0200

    Updated PublicLayout.vue clearing out AppLayout.vue

--- src/components/layouts/PublicLayout.vue
+++ src/components/layouts/PublicLayout.vue
@@ -11,7 +11,31 @@ export default {
 </script>
 
 <template>
-
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+    <div class="container">
+      <RouterLink class="navbar-brand" to="/">Kölcsönző</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/home">Kezdőlap</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/instruments">Hangszerek</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/instruments">Kölcsönzéseim</RouterLink>
+          </li>
+        </ul>
+        <div class="d-flex gap-2">
+          <RouterLink class="btn btn-outline-light btn-sm" to="/auth/login">Bejelentkezés</RouterLink>
+          <RouterLink class="btn btn-warning btn-sm" to="/register">Regisztráció</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
