79d25c28a6410582e6618ba26e36ab35a30cb875
user <user@DESKTOP-51547N5>
2026-04-09 19:35:11 +0200

    add InstrumentCard component with props and select event

--- src/components/instruments/InstrumentCard.vue
+++ src/components/instruments/InstrumentCard.vue
@@ -1,13 +1,26 @@
 <script >
 export default {
-  name:"InstrumentCard"
+  name:"InstrumentCard",
+  props:{
+    instrument:{type:Object, required: true , default: null}
+  }
 }
 </script>
 
 <template>
-
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title mb-1">{{instrument.name}}</h5>
+      <div class="text-muted mb-3">
+        - Kategória: {{ instrument.category }} - Márka: {{ instrument.brand }}
+      </div>
+      <div class="d-flex flex-wrap gap-2 mb-3">
+        <span>{{instrument.price}}</span>
+      </div>
+      <button type="button" class="btn btn-success w-100" @click="$emit('select', instrument.id)">Kölcsönzöm</button>
+    </div>
+  </div>
 </template>
-
 <style scoped>
 
 </style>
