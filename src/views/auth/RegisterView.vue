fa5dbd32e33ee8cb0ac66c3d2b3f71553d145ba1
user <user@DESKTOP-51547N5>
2026-04-09 19:41:11 +0200

    add RegisterView with form handling, csrf and API integration

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,11 +1,83 @@
 <script>
+import apiHandler from '../../utils/apiHandler';
+
 export default {
-  name: "RegisterView"
+  name:'register',
+  data() {
+    return {
+      form:{
+        email:'',
+        password:'',
+        password_confirmation:'',
+        title:'',
+        first_name:'',
+        last_name:''
+      }
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        await apiHandler.csrf();
+        const response = await apiHandler.register(JSON.stringify(this.form));
+        console.log(response);
+
+      } catch(err) {
+        console.log(err.response.data.errors);
+      } finally {
+
+      }
+    }
+  }
 }
 </script>
 
 <template>
+  <form>
+    <b class="d-block">Email cím</b>
+    <input
+        type="text" v-model="form.email"
+        class="form-control form-control-sm"
+    >
 
+    <b class="d-block">Jelszó</b>
+    <input
+        type="password" v-model="form.password"
+        class="form-control form-control-sm"
+    >
+
+    <b class="d-block">Jelszó újra</b>
+    <input
+        type="password" v-model="form.password_confirmation"
+        class="form-control form-control-sm"
+    >
+
+    <b class="d-block">Megszólítás</b>
+    <select v-model="form.title"
+            class="form-control form-control-sm">
+      <option value="-">válassz megszólítást</option>
+      <option value="Úr">Úr</option>
+      <option value="Hölgy">Hölgy</option>
+      <option value="Dr">Dr</option>
+      <option value="Prof">Prof</option>
+    </select>
+
+    <b class="d-block">Vezetéknév</b>
+    <input
+        type="text" v-model="form.first_name"
+        class="form-control form-control-sm"
+    >
+
+    <b class="d-block">Keresztnév</b>
+    <input
+        type="text" v-model="form.last_name"
+        class="form-control form-control-sm"
+    >
+
+    <button type="button" class="btn btn-primary btn-sm mt-2" @click="register()">
+      Regisztráció
+    </button>
+  </form>
 </template>
 
 <style scoped>
