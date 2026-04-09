aa81185c2a80bbe7839f14fabf0b9ae50f1fc06d
user <user@DESKTOP-51547N5>
2026-04-09 19:36:31 +0200

    add LendingList component to render and forward lending actions

--- src/components/lending/LendingList.vue
+++ src/components/lending/LendingList.vue
@@ -1,11 +1,20 @@
-<script >
+<script>
+import LendingItem from "@/components/lending/LendingItem.vue";
 export default {
-  name: "LendingListView"
+  name: "LendingList",
+  components: {LendingItem},
+  props: {
+    lendings: {type: Array, required: true, default: () => []},
+  },
+  emits: ["update",'remove'],
 }
 </script>
 
 <template>
-
+  <div class="list-group">
+    <LendingItem v-for="b in lendings" :key="b.id" :lending="b" @update="$emit('update',$event)" @remove="$emit('remove',$event)"
+    />
+  </div>
 </template>
 
 <style scoped>
