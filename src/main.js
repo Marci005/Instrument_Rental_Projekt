7d1918128681795c6a20232c2aa08107d85f7360
user <user@DESKTOP-51547N5>
2026-04-22 15:39:05 +0200

    imported css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import './assets/app.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
