ac5ac1a8096ba03275d9a5e0c2c9fddac05eccf4
user <user@DESKTOP-51547N5>
2026-04-22 15:37:58 +0200

    updated axios

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:8000',
-    withCredentials:true,
-    withXSRFToken:true,
+    baseURL: 'http://localhost:8000',
+    withCredentials: true,
+    withXSRFToken: true,
     headers: {
         'Accept': 'application/json',
-        'Content-Type':'application/json',
-        'X-Requested-With':'XMLHttpRequest'
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
     }
 });
 
